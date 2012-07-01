4bbde0fbe3d8be754bc861c6abfef4471e725370
Siebert Lubbe <siebert.lubbe@hubonline.com.au>
2012-07-01 19:13:55 +1000

    Deleting renamed files

--- lib/ability-controller.rb
+++
@@ -1,6 +0,0 @@
-require "ability-controller/version"
-
-module AbilityController
-  class Engine < Rails::Engine
-  end
-end
--- lib/ability-controller/version.rb
+++
@@ -1,5 +0,0 @@
-module Ability
-  module Controller
-    VERSION = "0.0.1"
-  end
-end
